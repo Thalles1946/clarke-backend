3c099132043c80783a2c83f60a82c230834dacb7
Thalles1946 <thallesaug2099@gmail.com>
2024-01-19 20:36:39 -0300

    Fix : Correção do tipo da query

--- main.py
+++ main.py
@@ -9,12 +9,11 @@ type_defs = """
     }
     type Empresa {
         nome: String!
-        logo: String!
-        estado: String!
-        custoKWh: Float!
-        minimoKWh: Float!
-        numeroClientes: Int!
-        avaliacao: Float!
+        estado_origem: String!
+        custo_por_kWh: Float!
+        limite_minimo_kWh: Float!
+        numero_total_clientes: Int!
+        avaliacao_media_clientes: Float!
     }
 """
 
@@ -27,7 +26,7 @@ def load_data(kWh):
     try:
         with open(nome_arquivo,'r') as arquivo_json:
             data = json.load(arquivo_json)
-        data["data"] = [empresa for empresa in data["data"] if empresa["limite_minimo_kWh"]>= int(kWh)]
+        data["data"] = [empresa for empresa in data["data"] if empresa["limite_minimo_kWh"] <= int(kWh)]
         data["data"] = sorted(data["data"], key=lambda x: x["avaliacao_media_clientes"], reverse=True)
         if data["data"] is not None:
             return data["data"]
@@ -59,29 +58,20 @@ schema = make_executable_schema(type_defs, query)
 
 app = Flask(__name__)
 
-# Retrieve HTML for the GraphiQL.
-# If explorer implements logic dependant on current request,
-# change the html(None) call to the html(request)
-# and move this line to the graphql_explorer function.
+
 explorer_html = ExplorerGraphiQL().html(None)
 
 
 @app.route("/graphql", methods=["GET"])
 def graphql_explorer():
-    # On GET request serve the GraphQL explorer.
-    # You don't have to provide the explorer if you don't want to
-    # but keep on mind this will not prohibit clients from
-    # exploring your API using desktop GraphQL explorer app.
+
     return explorer_html, 200
 
 
 @app.route("/graphql", methods=["POST"])
 def graphql_server():
-    # GraphQL queries are always sent as POST
     data = request.get_json()
 
-    # Note: Passing the request to the context is optional.
-    # In Flask, the current request is always accessible as flask.request
     success, result = graphql_sync(
         schema,
         data,
